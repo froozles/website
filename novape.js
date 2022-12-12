9750d533b273abf8f3fc208fa0730c3cb60b0ad3
froozles <117869023+froozles@users.noreply.github.com>
2022-12-11 20:28:22 -0400

    Add files via upload

---
+++ novape.js
@@ -0,0 +1,26 @@
+// @ts-ignore
+const { Typed } = window;
+
+const typed2 = new Typed('#strike', {
+  strings: [
+    ' ',
+    'a discordian',
+    'a closet cheater',
+    'an og robloxian',
+    'a kanye west lover',
+  ],
+  typeSpeed: 80,
+  backSpeed: 60,
+  smartBackspace: false,
+  loop: true,
+  shuffle: false,
+  backDelay: 2000,
+  startDelay: 3000,
+});
+
+// force the start of cursor animation while the `startDelay` is ticking
+if (typed2.cursor != null) {
+  // can't use `toggleBlinking(true)` here, as it has some extra checks
+  // whether animation has started...which defeats the purpose
+  typed2.cursor.classList.add('typed-cursor--blink');
+}
